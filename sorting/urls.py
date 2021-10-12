dbc9877a0c5c226af9825b9ef685ce6263b7e9b3
KAnggara <kanggara75@gmail.com>
2021-10-12 14:00:12 +0700

    add: finising

--- sorting/urls.py
+++ sorting/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('mergesort/', views.mergesort, name="mergesort"),
+    path('quicksort/', views.quicksort, name="quicksort"),
+    path('selectionsort/', views.selectionsort, name="selectionsort"),
+    path('bubblesort/', views.bubblesort, name="bubblesort"),
+    path('insertionsort/', views.insertionsort, name="insertionsort"),
 ]
