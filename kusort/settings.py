1fc59e8085648ce98044952553b9c29c76b4fd1d
KAnggara <kanggara75@gmail.com>
2021-10-14 00:50:01 +0700

    fix: static files css and js

--- kusort/settings.py
+++ kusort/settings.py
@@ -113,15 +113,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/'
-STATIC_ROOT= '/home/sopfurni/pyapps/kusort/public'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
-#STATIC_ROOT = (os.path.join(SITE_ROOT, 'staticfiles'))
-#SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
-#STATICFILES_DIRS = (os.path.join(SITE_ROOT, 'static/'),)
+STATIC_URL = '/static/'
+SITE_ROOT = os.path.dirname(os.path.realpath('./__file__'))
+STATIC_ROOT = (os.path.join(SITE_ROOT, 'staticfiles'))
+STATICFILES_DIRS = (os.path.join(SITE_ROOT, 'static/'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
